f35542f90f70ed3ab33e7dbd507b11fb94a51b14
Dmitry TTT <cybirgpl@gmail.com>
2025-03-24 22:45:11 +0700

    feat: modified vsc lsp-sample;

---
+++ commitlint.config.ts
@@ -0,0 +1,14 @@
+import type { UserConfig } from '@commitlint/types/lib/load';
+
+const config: UserConfig = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'test', 'chore', 'deps'],
+    ],
+  },
+};
+
+export default config;
